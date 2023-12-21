3590fd126fbccf7cce9a0c05c8637a6309378025
KarimE146 <karim666the@gmail.com>
2023-12-21 14:13:51 -0500

    changes

--- NearestNeighbor.hpp
+++ NearestNeighbor.hpp
@@ -16,18 +16,12 @@ class NODE {
 public:
     NODE(int id, double x, double y) : id_(id), x_(x), y_(y) {}
 
-    int getId(){ 
-            return id_; 
-        }
-    double getX(){ 
-            return x_; 
-        }
-    double getY(){ 
-            return y_; 
-        }
+    int getId() const { return id_; }
+    double getX() const { return x_; }
+    double getY() const { return y_; }
 
     static double distance(const NODE& a, const NODE& b) {
-        return sqrt((a.x_ - b.x_) * (a.x_ - b.x_) + (a.y_ - b.y_)  * (a.y_ - b.y_));
+        return std::hypot(a.x_ - b.x_, a.y_ - b.y_);
     }
 
 private:
@@ -36,11 +30,10 @@ private:
     double y_;
 };
 
-
-std::vector<NODE> read(const std::string &filename){
+std::vector<NODE> read(const std::string& filename) {
     std::ifstream file(filename);
     if (!file.is_open()) {
-        std::cerr << "Failed to open file: " << std::endl;
+        std::cerr << "Failed to open file: " << filename << std::endl;
         return {};
     }
 
@@ -51,47 +44,51 @@ std::vector<NODE> read(const std::string &filename){
 
     while (std::getline(file, line)) {
         std::istringstream iss(line);
-        if (!(iss >> id >> x >> y)) {
-            continue; 
+        if (iss >> id >> x >> y) {
+            nodes.emplace_back(id, x, y);
         }
-        nodes.emplace_back(id, x, y);
     }
 
     return nodes;
 }
 
-void nearestNeighbor(const std::string &filename) {
+void nearestNeighbor(const std::string& filename) {
     auto nodes = read(filename);
+    if (nodes.empty()) {
+        return;
+    }
+
     std::vector<bool> seen(nodes.size(), false);
     std::vector<int> path;
     double tot = 0.0;
-    auto current = nodes.begin();
-    path.push_back(current->getId());
-    seen[current - nodes.begin()] = true;
+
+    auto curr = nodes.begin();
+    path.push_back(curr->getId());
+    seen[curr - nodes.begin()] = true;
 
     auto start = std::chrono::high_resolution_clock::now();
 
     for (size_t i = 1; i < nodes.size(); ++i) {
         auto nearest = nodes.end();
-        double min = std::numeric_limits<double>::max();
+        double minDistance = std::numeric_limits<double>::max();
 
         for (auto it = nodes.begin(); it != nodes.end(); ++it) {
             if (!seen[it - nodes.begin()]) {
-                double distance = NODE::distance(*current, *it);
-                if (distance < min) {
+                double distance = NODE::distance(*curr, *it);
+                if (distance < minDistance) {
                     nearest = it;
-                    min = distance;
+                    minDistance = distance;
                 }
             }
         }
 
         seen[nearest - nodes.begin()] = true;
-        tot += min;
+        tot += minDistance;
         path.push_back(nearest->getId());
-        current = nearest;
+        curr = nearest;
     }
 
-    tot += NODE::distance(*current, nodes.front()); 
+    tot += NODE::distance(*curr, nodes.front());
     path.push_back(nodes.front().getId());
 
     auto end = std::chrono::high_resolution_clock::now();
@@ -100,9 +97,7 @@ void nearestNeighbor(const std::string &filename) {
     for (int id : path) {
         std::cout << id << " ";
     }
-    std::cout << std::endl;
-    std::cout << "Total Distance: " << tot << std::endl;
-    std::cout << "Time in ms: " << duration.count() << std::endl;
+    std::cout << "\nTotal Distance: " << tot << "\nTime in ms: " << duration.count() << std::endl;
 }
 
 //Total Distance: 1.03496e+06
